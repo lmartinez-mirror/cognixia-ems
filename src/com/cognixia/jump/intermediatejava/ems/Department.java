13e15f90232757cb1f4fee1b19f0cbb7bf8fee33
Luis Martinez <luis.martinez@disroot.org>
2021-12-13 17:05:08 -0600

    Department: return to class implementation

--- src/com/cognixia/jump/intermediatejava/ems/Department.java
+++ src/com/cognixia/jump/intermediatejava/ems/Department.java
@@ -1,52 +1,59 @@
 package com.cognixia.jump.intermediatejava.ems;
 
-enum Building {
-  A("Building A"),
-  B("Building B"),
-  C("Building C"),
-  D("Building D"),
-  E("Building E");
+import java.text.DecimalFormat;
 
-  private final String name;
+public class Department {
+  private String name;
+  private final String deptId;
+  private float budget;
+  private String phoneNumber;
+  private String building;
+  private Employee manager;
 
-  private Building(String name) {
+  private Department(DepartmentBuilder builder) {
+    this.name = builder.name;
+    this.deptId = builder.deptId;
+    this.budget = builder.budget;
+    this.phoneNumber = builder.phoneNumber;
+    this.building = builder.building;
+    this.manager = builder.manager;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
     this.name = name;
   }
 
-  @Override
-  public String toString() {
-    return name;
-  }
-}
-
-public enum Department {
-  HUMAN_RESOURCES(10000f, "800-283-9201", Building.A, null),
-  TECHNOLOGY(200000f, "800-829-9910", Building.B, null),
-  ACCOUNTING(50000f, "800-983-2831", Building.C, null),
-  ADMINISTRATION(500000f, "800-982-2913", Building.D, null);
-
-  private float budget;
-  private String phoneNumber;
-  private Building building;
-  private Employee manager;
-
-  Department(float budget, String phoneNumber, Building building, Employee manager) {
-    this.budget = budget;
-    this.phoneNumber = phoneNumber;
-    this.building = building;
-    this.manager = null;
+  // No setter
+  public String getDeptId() {
+    return deptId;
   }
 
   public float getBudget() {
     return budget;
   }
 
+  public void setBudget(float budget) {
+    this.budget = DepartmentBuilder.sanitizeBudget(budget);
+  }
+
   public String getPhoneNumber() {
     return phoneNumber;
   }
 
-  public Building getBuilding() {
+  public void setPhoneNumber(String phoneNumber) {
+    this.phoneNumber = phoneNumber;
+  }
+
+  public String getBuilding() {
     return building;
+  }
+
+  public void setBuilding(String building) {
+    this.building = building;
   }
 
   public Employee getManager() {
@@ -55,5 +62,58 @@ public enum Department {
 
   public void setManager(Employee manager) {
     this.manager = manager;
+  }
+
+  public static class DepartmentBuilder {
+    private String name;
+    private final String deptId;
+    private float budget;
+    private String phoneNumber;
+    private String building;
+    private Employee manager;
+
+    private static int counter = 10;
+    private static final DecimalFormat df = new DecimalFormat("#.00");
+
+    public DepartmentBuilder(String name) {
+      this.name = name;
+      this.deptId = Integer.toString(counter++);
+      this.budget = 0;
+      this.phoneNumber = null;
+      this.building = null;
+      this.manager = null;
+    }
+
+    public DepartmentBuilder budget(float budget) {
+      this.budget = sanitizeBudget(budget);
+      return this;
+    }
+
+    public DepartmentBuilder phoneNumber(String phoneNumber) {
+      this.phoneNumber = phoneNumber;
+      return this;
+    }
+
+    public DepartmentBuilder building(String building) {
+      this.building = building;
+      return this;
+    }
+
+    public DepartmentBuilder manager(Employee manager) {
+      this.manager = manager;
+      return this;
+    }
+
+    public Department build() {
+      return new Department(this);
+    }
+
+    private static float sanitizeBudget(float budget) throws IllegalArgumentException {
+      if (budget < 0) {
+        throw new IllegalArgumentException("budget cannot be negative");
+      }
+
+      return Float.parseFloat(df.format(budget));
+    }
   }
 }
