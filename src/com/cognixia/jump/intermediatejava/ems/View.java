a13d0e33d0436e9c6fb7a4f4efe6bbb10cfd33f9
Luis Martinez <luis.martinez@disroot.org>
2021-12-15 13:51:07 -0600

    View: add more checks, use StringBuilder instead

--- src/com/cognixia/jump/intermediatejava/ems/View.java
+++ src/com/cognixia/jump/intermediatejava/ems/View.java
@@ -4,41 +4,51 @@ import java.util.List;
 
 public abstract class View {
   private View() {}
+  private static final String d = System.lineSeparator();
 
   public static void printEmployee(Employee employee) {
-    System.out.println("====== EMPLOYEE ======");
-    StringBuilder sb = new StringBuilder();
-
-    sb.append("Name: " + employee.getName() + '\n');
-    sb.append("Employee ID: " + employee.getId() + '\n');
-    sb.append("Department: " + employee.getDepartment() + '\n');
-    sb.append("Employment Date: " + employee.getEmploymentDate() + '\n');
-    sb.append("Salary: " + employee.getSalary() + '\n');
+    StringBuilder sb = new StringBuilder("====== EMPLOYEE ======" + d)
+      .append("Name: " + employee.getName() + d)
+      .append("Age: " + employee.getAge() + d)
+      .append("Employee ID: " + employee.getId() + d)
+      .append("Department: " + employee.getDepartment() + d)
+      .append("Employment Date: " + employee.getEmploymentDate() + d)
+      .append("Salary: " + employee.getSalary() + d);
 
     System.out.println(sb.toString());
   }
 
   public static void printEmployees(List<Employee> employees) {
     System.out.println("====== EMPLOYEES ======");
+
+    if (employees.isEmpty()) {
+      System.out.println("there's nothing here");
+      return;
+    }
+
     employees.stream().forEach(System.out::println);
   }
 
   public static void printDepartment(Department department) {
-    System.out.println("====== DEPARTMENT ======");
-    StringBuilder sb = new StringBuilder();
-
-    sb.append("Name: " + department.getName() + '\n');
-    sb.append("Department ID: " + department.getDeptId() + '\n');
-    sb.append("Budget: " + department.getBudget() + '\n');
-    sb.append("Phone Number: " + department.getPhoneNumber() + '\n');
-    sb.append("Building: " + department.getBuilding() + '\n');
-    sb.append("Manager: " + department.getManager() + '\n');
+    StringBuilder sb = new StringBuilder("====== DEPARTMENT ======" + d)
+      .append("Name: " + department.getName() + d)
+      .append("Department ID: " + department.getDeptId() + d)
+      .append("Budget: " + department.getBudget() + d)
+      .append("Phone Number: " + department.getPhoneNumber() + d)
+      .append("Building: " + department.getBuilding() + d)
+      .append("Manager: " + department.getManager() + d);
 
     System.out.println(sb.toString());
   }
 
   public static void printDepartments(List<Department> departments) {
     System.out.println("====== DEPARTMENTS ======");
+
+    if (departments.isEmpty()) {
+      System.out.println("there's nothing here");
+      return;
+    }
+
     departments.stream().forEach(System.out::println);
   }
 }
