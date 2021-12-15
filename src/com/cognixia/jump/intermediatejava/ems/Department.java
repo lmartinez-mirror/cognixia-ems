5408de81fa40880a0703ac384e643d31bcbcddd5
Luis Martinez <luis.martinez@disroot.org>
2021-12-15 17:39:06 -0600

    Department: added javadocs

--- src/com/cognixia/jump/intermediatejava/ems/Department.java
+++ src/com/cognixia/jump/intermediatejava/ems/Department.java
@@ -1,15 +1,25 @@
 package com.cognixia.jump.intermediatejava.ems;
 
-import java.io.Serializable;
 import java.text.DecimalFormat;
 
-public class Department implements Serializable {
+/**
+ * Class for representing Departments within the EMS system
+ * @author Luis Martinez
+ */
+
+public class Department {
   private String name;
   private final String deptId;
   private float budget;
   private String phoneNumber;
   private String building;
   private Employee manager;
+
+  /**
+   * Private class constructor; use DepartmentBuilder to get Department
+   * instances instead of calling the constructor directly
+   * @param builder DepartmentBuilder object for creating Department instances
+   */
 
   private Department(DepartmentBuilder builder) {
     this.name = builder.name;
@@ -20,22 +30,41 @@ public class Department implements Serializable {
     this.manager = builder.manager;
   }
 
+  /**
+   * @return Department name
+   */
+
   public String getName() {
     return name;
   }
+
+  /**
+   * @param name Name to rename Department to
+   */
 
   public void setName(String name) {
     this.name = name;
   }
 
-  // No setter
+  /**
+   * @return Department's ID
+   */
+
   public String getDeptId() {
     return deptId;
   }
 
+  /**
+   * @return Department's budget
+   */
+
   public float getBudget() {
     return budget;
   }
+
+  /**
+   * @param budget Amount to set Department's budget to
+   */
 
   public void setBudget(float budget) {
     try {
@@ -45,25 +74,49 @@ public class Department implements Serializable {
     }
   }
 
+  /**
+   * @return Department's phone number
+   */
+
   public String getPhoneNumber() {
     return phoneNumber;
   }
+
+  /**
+   * @param phoneNumber String to set Department's phone number to
+   */
 
   public void setPhoneNumber(String phoneNumber) {
     this.phoneNumber = phoneNumber;
   }
 
+  /**
+   * @return Department's building as a String
+   */
+
   public String getBuilding() {
     return building;
   }
+
+  /**
+   * @param Department's assigned building
+   */
 
   public void setBuilding(String building) {
     this.building = building;
   }
 
+  /**
+   * @return Department's manager as an Employee object
+   */
+
   public Employee getManager() {
     return manager;
   }
+
+  /**
+   * @param manager New manager to assign to the Department
+   */
 
   public void setManager(Employee manager) {
     this.manager = manager;
@@ -83,6 +136,14 @@ public class Department implements Serializable {
     return sb.toString();
   }
 
+  /**
+   * Builder for Department class
+   * <p>
+   * Use this class to create Department instances instead of directly invoking
+   * the constructor
+   * @author Luis Martinez
+   */
+
   public static class DepartmentBuilder {
     private String name;
     private final String deptId;
@@ -91,6 +152,7 @@ public class Department implements Serializable {
     private String building;
     private Employee manager;
 
+    /** Tracks employee IDs, ensures no two are the same */
     private static int counter = 10;
     private static final DecimalFormat df = new DecimalFormat("#.00");
 
@@ -103,6 +165,13 @@ public class Department implements Serializable {
       this.manager = null;
     }
 
+    /**
+     * @param budget Amount to set pending Department object's budget to
+     * @return Calling Builder object
+     * @apiNote Intermediate method: Chain this method to other intermediate
+     * methods to combine results.
+     */
+
     public DepartmentBuilder budget(float budget) {
       try {
         this.budget = sanitizeBudget(budget);
@@ -112,24 +181,59 @@ public class Department implements Serializable {
       return this;
     }
 
+    /**
+     * @param phoneNumber String to assign pending Department object's phone
+     * number to
+     * @return Calling Builder object
+     * @apiNote Intermediate method: Chain this method to other intermediate
+     * methods to combine results.
+     */
+
     public DepartmentBuilder phoneNumber(String phoneNumber) {
       this.phoneNumber = phoneNumber;
       return this;
     }
+
+    /**
+     * @param building String to assign pending Department object's building to
+     * @return Calling Builder object
+     * @apiNote Intermediate method: Chain this method to other intermediate
+     * methods to combine results.
+     */
 
     public DepartmentBuilder building(String building) {
       this.building = building;
       return this;
     }
 
+    /**
+     * @param manager Department's new manager
+     * @return Calling Builder object
+     * @apiNote Intermediate method: Chain this method to other intermediate
+     * methods to combine results.
+     */
+
     public DepartmentBuilder manager(Employee manager) {
       this.manager = manager;
       return this;
     }
 
+    /**
+     * @return A new Department object using Builder object's intermediate data
+     * @apiNote Terminal method: Use this method to finish a chain of
+     * intermediate method calls and yield a complete product.
+     */
+
     public Department build() {
       return new Department(this);
     }
+
+    /**
+     * Helper method for formatting budget to USD
+     * @param budget Amount to change Department's budget to
+     * @return budget after processing
+     * @throws EMSNegativeFundsException If budget is less than zero
+     */
 
     public static float sanitizeBudget(float budget) throws EMSNegativeFundsException {
       if (budget < 0) {
