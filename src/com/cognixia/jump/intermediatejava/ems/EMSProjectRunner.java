0e95d41744ad9f29fe23efa59d6b00088313e3cc
Luis Martinez <luis.martinez@disroot.org>
2021-12-14 23:56:01 -0600

    EMSProjectRunner: first draft

---
+++ src/com/cognixia/jump/intermediatejava/ems/EMSProjectRunner.java
@@ -0,0 +1,14 @@
+package com.cognixia.jump.intermediatejava.ems;
+
+public class EMSProjectRunner {
+
+  public static void main(String[] args) {
+    Controller controller = Controller.getInstance();
+
+    System.out.println("====== EMPLOYEE MANAGEMENT SYSTEM 2022 EDITION ======\n\n");
+
+    while (true) {
+      System.out.println("Main Menu\n=========");
+    }
+  }
+}
