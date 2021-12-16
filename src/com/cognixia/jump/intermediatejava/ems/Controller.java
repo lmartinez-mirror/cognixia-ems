f4fe3d246e560b4672132aba0d84033f21565dd0
Luis Martinez <luis.martinez@disroot.org>
2021-12-15 20:24:56 -0600

    Controller: add javadocs

--- src/com/cognixia/jump/intermediatejava/ems/Controller.java
+++ src/com/cognixia/jump/intermediatejava/ems/Controller.java
@@ -9,39 +9,74 @@ import java.util.Scanner;
 import com.cognixia.jump.intermediatejava.ems.Department.DepartmentBuilder;
 import com.cognixia.jump.intermediatejava.ems.Employee.EmployeeBuilder;
 
+/**
+ * Controller class for EMS
+ * <p>
+ * This program uses a model-view-controller (MVC) paradigm to separate view logic from
+ * I/O and interaction logic
+ * @author Luis Martinez
+ */
+
 public class Controller {
   private static final Controller instance = new Controller();
   private List<Employee> employees = new ArrayList<Employee>();
   private List<Department> departments = new ArrayList<Department>();
   public Scanner scanner = new Scanner(System.in);
 
+  /**
+   * Private constructor to enforce Singleton pattern
+   */
+
   private Controller() {}
+
+  /**
+   * @return Static instance of Controller object
+   * @apiNote Implements Singleton pattern
+   */
 
   public static Controller getInstance() {
     return instance;
   }
 
+  /**
+   * Calls View class to print list of Employees
+   */
+
   public void printEmployees() {
     View.printEmployees(employees);
   }
 
+  /**
+   * Calls View class to print list of Departments
+   */
+
   public void printDepartments() {
     View.printDepartments(departments);
   }
+
+  /**
+   * Helper method for getting a single Employee with a user prompt
+   * @return Employee object, or null if getEmployeeIndex returns -1
+   */
 
   private Employee getEmployee() {
     Employee target = null;
     int idx = getEmployeeIndex();
 
     try {
-      target = employees.get(idx);
-    }
-    catch (IndexOutOfBoundsException e) {
+      target = employees.get(idx - 1);
+    } catch (IndexOutOfBoundsException e) {
       System.out.println(e);
     }
 
     return target;
   }
+
+  /**
+   * Helper method for getEmployee, handles index logic
+   * @return Index of Employee within the list of Employees, or -1 if no
+   * Employees are available at the specified index
+   */
 
   private int getEmployeeIndex() {
     int idx = -1;
@@ -56,22 +91,18 @@ public class Controller {
     try {
       idx = scanner.nextInt();
       scanner.nextLine();
-
-      /* if (idx < 0 || idx > employees.size()) {
-        throw new IndexOutOfBoundsException("index not within range");
-      } */
-    }
-    catch (InputMismatchException e) {
+    } catch (InputMismatchException e) {
       System.out.println(e);
       return -1;
     }
-    /* catch (IndexOutOfBoundsException e) {
-      System.out.println(e);
-      return -1;
-    } */
 
     return idx;
   }
+
+  /**
+   * Controller method for Employee creation. Top-level logic should go here;
+   * call the method within the main loop.
+   */
 
   public void createEmployee() {
     System.out.println("====== CREATE EMPLOYEE ======");
@@ -102,11 +133,15 @@ public class Controller {
       Employee employee = builder.build();
       employees.add(employee);
       System.out.println("Added employee " + employee);
-    }
-    catch (Exception e) {
+    } catch (Exception e) {
       System.out.println("failed to create employee:  " + e);
     }
   }
+
+  /**
+   * Controller method for reading Employee data. Top-level logic should go
+   * here; call the method within the main loop.
+   */
 
   public void readEmployee() {
     if (employees.isEmpty()) {
@@ -120,6 +155,11 @@ public class Controller {
       View.printEmployee(employee);
     }
   }
+
+  /**
+   * Controller method for updating employee data. Top-level logic should go
+   * here; call the method within the main loop.
+   */
 
   public void updateEmployee() {
     if (employees.isEmpty()) {
@@ -148,11 +188,9 @@ public class Controller {
     try {
       idx = scanner.nextInt();
       scanner.nextLine();
-    }
-    catch (InputMismatchException e) {
+    } catch (InputMismatchException e) {
       System.out.println(e);
-    }
-    finally {
+    } finally {
       switch (idx) {
         case 1: {
           Department department = getDepartment();
@@ -167,8 +205,7 @@ public class Controller {
           try {
             salary = scanner.nextFloat();
             scanner.nextLine();
-          }
-          catch (InputMismatchException e) {
+          } catch (InputMismatchException e) {
             System.out.println("could not update salary:  " + e);
             return;
           }
@@ -179,6 +216,11 @@ public class Controller {
       }
     }
   }
+
+  /**
+   * Controller method for removing an Employee. Top-level logic should go
+   * here; call the method within the main loop.
+   */
 
   public void removeEmployee() {
     if (employees.isEmpty()) {
@@ -197,9 +239,18 @@ public class Controller {
     }
   }
 
+  /**
+   * Controller method for removing all Employees. Top-level logic should go
+   * here; call the method within the main loop.
+   */
+
   public void removeAllEmployees() {
     removeAll(employees);
   }
+
+  /**
+   * Helper method for getting Department index within the Departments list.
+   */
 
   private int getDepartmentIndex() {
     int idx = -1;
@@ -214,13 +265,19 @@ public class Controller {
     try {
       idx = scanner.nextInt();
       scanner.nextLine();
-    }
-    catch (InputMismatchException e) {
+    } catch (InputMismatchException e) {
       System.out.println(e);
     }
 
     return idx;
   }
+
+  /**
+   * Helper method for getting a Department within the Departments list;
+   * @return Reference to target Department object
+   * <p>
+   * Null if list is empty
+   */
 
   private Department getDepartment() {
     if (departments.isEmpty()) {
@@ -236,12 +293,16 @@ public class Controller {
       int idx = scanner.nextInt();
       scanner.nextLine();
       target = departments.get(idx);
-    }
-    catch (Exception e) {
+    } catch (Exception e) {
       System.out.println("failed to fetch department: " + e);
     }
     return target;
   }
+
+  /**
+   * Controller method for creating Departments. Top-level logic should go
+   * here; call the method within the main loop.
+   */
 
   public void createDepartment() {
     System.out.println("====== CREATE DEPARTMENT =====");
@@ -251,11 +312,14 @@ public class Controller {
       builder = null;
       System.out.print("Name:  ");
       builder = new DepartmentBuilder(scanner.nextLine());
+
       System.out.print("Budget:  ");
       builder.budget(scanner.nextFloat());
       scanner.nextLine();
+
       System.out.print("Phone Number:  ");
       builder.phoneNumber(scanner.nextLine());
+
       System.out.print("Building:  ");
       builder.building(scanner.nextLine());
 
@@ -264,12 +328,15 @@ public class Controller {
       }
 
       departments.add(builder.build());
-    }
-
-    catch (InputMismatchException e) {
+    } catch (InputMismatchException e) {
       System.out.println(e);
     }
   }
+
+  /**
+   * Controller method for reading Department data. Top-level logic should go
+   * here; call the method within the main loop.
+   */
 
   public void readDepartment() {
     if (departments.isEmpty()) {
@@ -284,6 +351,11 @@ public class Controller {
     }
   }
 
+  /**
+   * Controller method for updating Department data. Top-level logic should go
+   * here; call the method within the main loop.
+   */
+
   public void updateDepartment() {
     if (departments.isEmpty()) {
       System.out.println("No departments available");
@@ -294,6 +366,12 @@ public class Controller {
     Department department = getDepartment();
     updateDepartment(department);
   }
+
+  /**
+   * Helper method for updateDepartment. This processes a single Department
+   * for targeting.
+   * @param department Target Department to change attribute data for
+   */
 
   private void updateDepartment(Department department) {
     if (department == null) {
@@ -313,8 +391,7 @@ public class Controller {
     try {
       idx = scanner.nextInt();
       scanner.nextLine();
-    }
-    catch (InputMismatchException e) {
+    } catch (InputMismatchException e) {
       System.out.println(e);
     }
     finally {
@@ -325,8 +402,7 @@ public class Controller {
           try {
             budget = scanner.nextFloat();
             scanner.nextLine();
-          }
-          catch (InputMismatchException e) {
+          } catch (InputMismatchException e) {
             System.out.println("could not update budget:  " + e);
             return;
           }
@@ -342,8 +418,7 @@ public class Controller {
 
           try {
             phoneNumber = scanner.nextLine();
-          }
-          catch (InputMismatchException e) {
+          } catch (InputMismatchException e) {
             System.out.println("could not update phone number: " + e);
             return;
           }
@@ -359,8 +434,7 @@ public class Controller {
 
           try {
             building = scanner.nextLine();
-          }
-          catch (InputMismatchException e) {
+          } catch (InputMismatchException e) {
             System.out.println("could not update building: " + e);
             return;
           }
@@ -386,6 +460,11 @@ public class Controller {
     }
   }
 
+  /**
+   * Controller method for removing Departments. Top-level logic should go
+   * here; call the method within the main loop.
+   */
+
   public void removeDepartment() {
     if (departments.isEmpty()) {
       System.out.println("No departments available");
@@ -403,9 +482,19 @@ public class Controller {
     }
   }
 
+  /**
+   * What it says on the tin.
+   */
+
   public void removeAllDepartments() {
     removeAll(departments);
   }
+
+  /**
+   * Clears out both Employees and Departments lists.
+   * <p>
+   * Use this if you want to get fired.
+   */
 
   public void removeAll() {
     System.out.println("this is a destructive operation, are you sure?  y/n");
@@ -413,8 +502,7 @@ public class Controller {
 
     try {
       response = scanner.nextLine();
-    }
-    catch (InputMismatchException e) {
+    } catch (InputMismatchException e) {
       System.out.println(e);
       return;
     }
@@ -426,14 +514,20 @@ public class Controller {
     }
   }
 
+  /**
+   * For clearing out specific lists
+   * @param <T> Employee
+   * @param <T> Department
+   * @param list Either of the two Controller lists
+   */
+
   public <T> void removeAll(List<T> list) {
     System.out.println("this is a destructive operation, are you sure?  y/n");
     String response = null;
 
     try {
       response = scanner.nextLine();
-    }
-    catch (InputMismatchException e) {
+    } catch (InputMismatchException e) {
       System.out.println(e);
       return;
     }
