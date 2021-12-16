da951d5848940b92eecdaa5c6cb97ced8abbd964
Luis Martinez <luis.martinez@disroot.org>
2021-12-15 21:38:15 -0600

    EMSProjectRunner: linting

--- src/com/cognixia/jump/intermediatejava/ems/EMSProjectRunner.java
+++ src/com/cognixia/jump/intermediatejava/ems/EMSProjectRunner.java
@@ -9,27 +9,21 @@ public class EMSProjectRunner {
     boolean flag = true;
     System.out.println("====== EMPLOYEE MANAGEMENT SYSTEM 2022 EDITION ======\n\n");
 
-    StringBuilder sb = new StringBuilder("Main Menu\n==========\n");
-    sb.append("(1) Create Employee\n");
-    sb.append("(2) Read Employee\n");
-    sb.append("(3) Read All Employees\n");
-    sb.append("(4) Update Employee\n");
-    sb.append("(5) Delete Employee\n");
-    sb.append("(6) Delete All Employees\n");
-    sb.append("(7) Create Department\n");
-    sb.append("(8) Read Department\n");
-    sb.append("(9) Read All Departments\n");
-    sb.append("(10) Update Department\n");
-    sb.append("(11) Delete Department\n");
-    sb.append("(12) Delete All Departments\n");
-    sb.append("\n\n");
-    sb.append("(99) Burn It All Down\n");
+    // Please forgive this godawful formatting, this is apparently within Google style guidelines
+    final String menu = new StringBuilder("Main Menu\n==========\n").append("(1) Create Employee\n")
+        .append("(2) Read Employee\n").append("(3) Read All Employees\n")
+        .append("(4) Update Employee\n").append("(5) Delete Employee\n")
+        .append("(6) Delete All Employees\n").append("(7) Create Department\n")
+        .append("(8) Read Department\n").append("(9) Read All Departments\n")
+        .append("(10) Update Department\n").append("(11) Delete Department\n")
+        .append("(12) Delete All Departments\n").append("\n\n").append("(99) Burn It All Down\n")
+        .toString();
 
     while (flag) {
       int choice = 0;
 
-      System.out.println(sb.toString());
-      System.out.print("make a selection:  ");
+      System.out.println(menu);
+      System.out.print("Make a selection:  ");
 
       try {
         choice = controller.scanner.nextInt();
@@ -39,23 +33,50 @@ public class EMSProjectRunner {
       }
 
       switch (choice) {
-        case 1: controller.createEmployee(); break;
-        case 2: controller.readEmployee(); break;
-        case 3: controller.printEmployees(); break;
-        case 4: controller.updateEmployee(); break;
-        case 5: controller.removeEmployee(); break;
-        case 6: controller.removeAllEmployees(); break;
-        case 7: controller.createDepartment(); break;
-        case 8: controller.readDepartment(); break;
-        case 9: controller.printDepartments(); break;
-        case 10: controller.updateDepartment(); break;
-        case 11: controller.removeDepartment(); break;
-        case 12: controller.removeAllDepartments(); break;
-        case 99: controller.removeAll(); break;
-        case 0: flag = false;
+        case 1:
+          controller.createEmployee();
+          break;
+        case 2:
+          controller.readEmployee();
+          break;
+        case 3:
+          controller.printEmployees();
+          break;
+        case 4:
+          controller.updateEmployee();
+          break;
+        case 5:
+          controller.removeEmployee();
+          break;
+        case 6:
+          controller.removeAllEmployees();
+          break;
+        case 7:
+          controller.createDepartment();
+          break;
+        case 8:
+          controller.readDepartment();
+          break;
+        case 9:
+          controller.printDepartments();
+          break;
+        case 10:
+          controller.updateDepartment();
+          break;
+        case 11:
+          controller.removeDepartment();
+          break;
+        case 12:
+          controller.removeAllDepartments();
+          break;
+        case 99:
+          controller.removeAll();
+          break;
+        case 0:
+          flag = false;
       }
 
-      System.out.println("\n\n");
+      System.out.println("\n");
     }
 
     System.out.println("goodbye");
